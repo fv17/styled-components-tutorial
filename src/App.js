16da2473adbe79b116bd0bd28f353c47fe1de46d
fv17 <forest.valley0417@gmail.com>
2019-07-15 16:53:47 +0900

    feat: titleの追加

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Button } from './styledButton'
+import { Title } from './styledTitle'
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div>
-      Hello!
-    </div>
+    <Container>
+      <Title>Sample Title</Title>
+      <Button>Normal button</Button>
+      <Button primary>Primary button</Button>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  background: papayawhip;
+  padding: 4em;
+  text-align: center;
+`
 
 export default App;
